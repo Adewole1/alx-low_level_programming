a2b826c713a531730c6902ab15ab60fb4c7b9100
Adewole1 <madedayo@gmail.com>
2023-03-16 12:00:40 -0700

    Print using printf to stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - using printf() to print
+ *
+ * Description: This uses
+ * printf function to print
+ * Return: it returns integer 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
