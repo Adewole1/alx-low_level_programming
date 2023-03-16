ecaa5be8637ede733f8aaa9694441f61ade42e85
Adewole1 <madedayo@gmail.com>
2023-03-16 12:41:26 -0700

    printing to stderr rather than stdout

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - using fprintf() to print
+ *
+ * Description: This uses
+ * fprintf function to print
+ * to stderr
+ * Return: it returns integer 1 for error
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
