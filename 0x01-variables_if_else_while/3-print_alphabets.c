98dc410cec2e5c26e10d9577e2febdb1b2706098
Adewole1 <madedayo@gmail.com>
2023-03-17 14:07:10 -0700

    Print lowercase and uppercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Function to Print the alphabet line by line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar('\n');
+	return (0);
+}
