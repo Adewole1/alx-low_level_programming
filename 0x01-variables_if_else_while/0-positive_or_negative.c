651143176acf76d45e90f786232c20c2f265ffa8
Adewole1 <madedayo@gmail.com>
2023-03-17 13:22:11 -0700

    Check if number is greater or lesser than zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - using printif() to print a conditional
+ *
+ * Description: This checks if
+ * a variable is more than ore less than
+ * or equal to zero
+ * Return: it returns integer 0
+ */
+int main(n)
+{
+	if (n>0)
+	{
+		printf("%d is positive\n", n);
+	}else if (n<0)
+	{
+		printf("%d is negative\n", n);
+	} else
+	{
+		printf("%d is zero", n);
+
+	return (0);
+}
