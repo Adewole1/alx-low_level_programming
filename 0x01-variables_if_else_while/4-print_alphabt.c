d5d089572ad76ce66d3ea2ca59c3acaadc53d357
Adewole1 <madedayo@gmail.com>
2023-03-17 14:15:11 -0700

    Print lowercase letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Function to Print the alphabet line by line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if ((i != q) && (i != e))
+		{
+			putchar(alphabets[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
