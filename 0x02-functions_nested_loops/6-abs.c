f04103c4e81209a19d3bb70f98d2db45635183f3
Adewole1 <madedayo@gmail.com>
2023-03-21 15:47:40 -0700

    Print absolute of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int_abs - Function to absolut of a number
+ *
+ * @n: number to be checked
+ *
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
