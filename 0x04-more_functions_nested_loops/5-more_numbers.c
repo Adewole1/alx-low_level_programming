f0fa8b5064c05252c99ea14038f045657c395bc4
Adewole1 <madedayo@gmail.com>
2023-03-23 09:36:13 -0700

    Print 1 - 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers with putchar
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15, j++)
+		{
+			if (j > 20)
+			{
+				_putchar('1');
+			}
+			_putchar(j % 10 + '0');
+		}
+	}
+
+	_putchar('\n');
+}
