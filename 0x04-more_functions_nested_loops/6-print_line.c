f9675af87043ad94ee494181b4e389dbb204a041
Adewole1 <madedayo@gmail.com>
2023-03-23 09:53:57 -0700

    Print straight line with underscore

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Print the underscore
+ *
+ * @n: Number of times to print
+ *
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+	}
+
+	_putchar('\n');
+}
