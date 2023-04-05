f33e029c58cc9444e84a8d3fad68dcf52a7412c7
Adewole1 <madedayo@gmail.com>
2023-04-05 16:56:29 -0700

    Function to multiply a number recursively

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Function to multiply a number recursively
+ *
+ * @x: number to multiply
+ * @y: power
+ *
+ * Return: positive integer, -1 if negative number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (x > 0)
+	{
+		if (y == 1)
+			return (x);
+		return (x * factorial(y - 1));
+	}
+
+	return (-1);
+}
