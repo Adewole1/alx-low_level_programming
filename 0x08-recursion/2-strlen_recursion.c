193fdebb480854f4973a4b9915e23f604643d62d
Adewole1 <madedayo@gmail.com>
2023-04-05 16:32:00 -0700

    Get the length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to recursibely count length of string
+ *
+ * @s: string to count
+ *
+ * Return: count
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+	if (*s != '\0')
+	{
+		return (1);
+	}
+	
+	return (count + _strlen_recursion(s + 1));
+}
