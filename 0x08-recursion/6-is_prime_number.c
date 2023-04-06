ef4fd4be56e945ac7731c62d08716c9f303a1a04
Adewole1 <madedayo@gmail.com>
2023-04-05 17:29:12 -0700

    Function to get prime number using recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * is_prime - check from 2 upward
+ *
+ * @n: number to check
+ * @i: step increment
+ *
+ * Return: integer
+ */
+
+int is_prime(int n, int i)
+{
+	if (i > n || n % i == 0)
+		return (0);
+	if (i == n)
+		return (1);
+	return (is_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - Check if a number is prime
+ *
+ * @n: number to check
+ *
+ * Return: integer
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 2)
+		return (0);
+	return (is_prime(n, 2));
+}
