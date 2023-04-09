d71ad858ae43121e8485e327a02368ad8e5bbd70
Adewole1 <madedayo@gmail.com>
2023-04-09 15:16:26 -0700

    Function to add integer arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - function to print it's name in std
@@ -12,13 +13,13 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, sum = - 0;
 
 	if (argc >= 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0 && argv[i] != 0)
+			if (atoi(argv[i]) == 0 && strcmp(argv[i], "0") != 0)
 			{
 				printf("Error\n");
 				return (1);
