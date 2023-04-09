584c6f6731e8babfd6e261a69b15ec2455a6d561
Adewole1 <madedayo@gmail.com>
2023-04-09 14:20:13 -0700

    Function to multiply 2 arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,9 +15,9 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (1);
+		return (0);
 	}
 
 	printf("Error\n");
-	return (0);
+	return (1);
 }
