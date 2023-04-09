c5f5f4c9477165e44de9fd5225c9bdcec8edd71a
Adewole1 <madedayo@gmail.com>
2023-04-09 13:54:41 -0700

    Function to print argument vector

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function to print it's name in std
+ *
+ * @argc: argument count
+ * @argv: array of arguments passed
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
