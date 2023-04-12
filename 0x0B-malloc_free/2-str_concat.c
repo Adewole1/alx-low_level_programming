3f58750206ff9e4789326a8a2aaebaf88e8ae7db
Adewole1 <madedayo@gmail.com>
2023-04-12 12:32:40 -0700

    Function to contatenate 2 strings in new allocated memory

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to recursively count length of string
+ *
+ * @str: string to count
+ *
+ * Return: count
+ */
+
+int _strlen_recursion(char *str)
+{
+	int count = 0;
+
+	if (*str != '\0')
+	{
+		count++;
+		count += _strlen_recursion(str + 1);
+	}
+
+	return (count);
+}
+
+/**
+ * _strconcat - function to create array of characters to allocated memory
+ *
+ * @s1: string 1 to fill memory with
+ * @s2: string 2 to fill memory with
+ *
+ * Return: pointer to new array
+ */
+
+char *_strconcat(char *s1, char *s2)
+{
+	char *new_str;
+	unsigned int i = 0, j = 0;
+	unsigned int size = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	size += _strlen_recursion(s1);
+	size += _strlen_recursion(s2);
+
+	new_str = (char *)malloc(size * sizeof(char) + 1);
+
+	if (new_str == NULL)
+		return (NULL);
+
+	while (i < size && s1[i] != '\0')
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+
+	while ((i + j) < size && s2[j] != '\0')
+	{
+		new_str[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	return (new_str);
+}
