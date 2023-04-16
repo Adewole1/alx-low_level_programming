6c3db2bc92eff91377b838fa5d4cfb4967b9825a
Adewole1 <madedayo@gmail.com>
2023-04-16 08:00:29 -0700

    Function to allocate memory based on size

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _calloc - Function to allocate memory and fill it with 0
+ *
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: Pointer to new array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	unsigned int i, total_size;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total_size = nmemb * size;
+
+	mem = malloc(total_size);
+
+	if (mem == NULL)
+		return (NULL);
+
+	for (i = 0; i < total_size; i++)
+		mem[i] = 0;
+
+	return (mem);
+}
