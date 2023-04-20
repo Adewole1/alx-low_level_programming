c49bd7c33c6a49fc81cf10a69a91c556cb9b9fbf
Adewole1 <madedayo@gmail.com>
2023-04-20 15:55:16 -0700

    Variadic Function to print different type of arguments

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - Variadic function to print all types of arguments
+ *
+ * @format: format of argument to print
+ *
+ * Return: Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args_list;
+	char *string, *separator = "";
+	unsigned int i = 0;
+
+	va_start(args_list, n);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 's':
+					string = va_arg(args_list, char *);
+					if (string == NULL || !string)
+						printf("%s(nil)", separator);
+					else
+						printf("%s%s", separator, string);
+					break;
+				case 'c':
+					printf("%s%c", separator, va_arg(args_list, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(args_list, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(args_list, double));
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(args_list);
+}
