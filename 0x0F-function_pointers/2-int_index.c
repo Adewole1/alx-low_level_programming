e5ea11c5333a7bbb9876a927dea6b0c9550388df
Adewole1 <madedayo@gmail.com>
2023-04-20 04:37:03 -0700

    Function to compare/find element

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - Function to print if an integer is in an array
+ *
+ * @array: array to search
+ * @size: size of array
+ * @cmp: function to use to conpare
+ *
+ * Return: nothing
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || !size || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		res = cmp(array[i]);
+
+		if (res != 0)
+			return (i);
+	}
+
+	return (-1);
+}
