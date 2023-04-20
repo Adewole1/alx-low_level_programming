c62efb85e63d78999f88b527a501e873907f95bc
Adewole1 <madedayo@gmail.com>
2023-04-20 04:16:50 -0700

    Function to pass elrments5o function ftom array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL || size == NULL || action == NULL)
+	if (array == NULL || !size || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
