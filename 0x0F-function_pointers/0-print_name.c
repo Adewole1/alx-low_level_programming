8ac278bd53dcf6327c13f2fc1f3bf02dbefdb899
Adewole1 <madedayo@gmail.com>
2023-04-20 03:24:54 -0700

    Function to print string from pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_name - Function to print based on another function
+ *
+ * @name: name to print
+ * @f: function to call
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks for ALX C-Function pointers
