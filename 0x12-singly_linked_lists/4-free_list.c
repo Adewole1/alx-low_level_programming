e145996b65d74becb00a7221f8f12be821df5253
Adewole1 <madedayo@gmail.com>
2023-04-27 21:30:01 -0700

    Function to free memory allocated with singly list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - Function to free memore allocated yo a linked list
+ *
+ * @head: The first node of the linked list
+ *
+ * Return: nothing
+ */
+void free_list(const list_t *head)
+{
+	list_t *node = NULL;
+
+	while (head != NULL)
+	{
+		node = head->next;
+		free(head->str);
+		free(head);
+		head = node;
+	}
+}
