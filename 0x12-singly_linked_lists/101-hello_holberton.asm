a8a75a4c1da8b9385e9a8334ee9f609bf9a208d5
Adewole1 <madedayo@gmail.com>
2023-04-27 22:35:31 -0700

    Hello in assembly language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,11 @@
-        global   main
+greeting: db 'Hello, Holberton\n'
 
-          extern    printf
+	global   main
+	extern    printf
 
 main:
-
-          mov   edi, greetings
-
-          xor   eax, eax
-
-          call  printf
-
-          mov   eax, 0
-
-          ret
-
-greetings: db `Hello, Holberton\n`,
+	mov   edi, greeting
+	xor   eax, eax
+	call  printf
+	mov   eax, 0
+	ret
