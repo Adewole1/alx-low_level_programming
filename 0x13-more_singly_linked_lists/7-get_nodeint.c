e2056aacb78801b94308c0cc3e030a3ee4c50d77
Adewole1 <madedayo@gmail.com>
2023-05-02 05:30:12 -0700

    Function to get a node in llinked list based on index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Get nth node of a list
+ *
+ * @head: The first data of the linked list
+ * @index: nth element to get
+ *
+ * Return: the node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *node = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL && count < index)
+	{
+		count++;
+		head = head->next;
+		node = head->next;
+	}
+
+	return (node);
+}
