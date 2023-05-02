e1ed15194bf08f7cd85d0b9f36ee02046803c0eb
Adewole1 <madedayo@gmail.com>
2023-05-02 04:30:51 -0700

    Function to free stringly linked list and repleace head with null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function to free memore allocated yo a linked list
+ *
+ * @head: The first node of the linked list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node = NULL;
+
+	while (head != NULL)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+
+	*head = NULL;
+}
